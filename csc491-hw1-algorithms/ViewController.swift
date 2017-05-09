15fb9bfdde4f9dfdc0f4280c07e1194e84f563d4
lewtech <lewtech@gmail.com>
2017-05-09 05:54:33 -0500

    initial commit uicollectionview

--- csc491-hw1-algorithms/ViewController.swift
+++ csc491-hw1-algorithms/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellId = "cellId"
+
+    let values: [CGFloat] = [20,30,40,50,200]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView?.backgroundColor = .white
+
+        collectionView?.register(BarCell.self, forCellWithReuseIdentifier: cellId)
+
+        (collectionView?.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return values.count
+    }
+
+    func maxHeight() -> CGFloat {
+        return view.frame.height - 20 - 44 - 8
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! BarCell
+
+        if let max = values.max() {
+            let value = values[indexPath.item]
+            let ratio = value / max
+
+            cell.barHeightConstraint?.constant = maxHeight() * ratio
+        }
+
+        return cell
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 30, height: maxHeight())
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+    }
 
 }
 
